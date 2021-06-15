386ff37f0bb2333bd4a26bf163806545eba88831
Jake <jake.a.reddy@gmail.com>
2021-06-15 12:21:32 +0300

    added text varibles to cv so i can later implement langauge toggle

---
+++ src/assets/text/cvText.js
@@ -0,0 +1,55 @@
+export const cvContentText = {
+  english: {
+    profile: {
+      title: 'Profile',
+      content: `Passionate self-taught Javascript developer with experiance in customer relations and management.
+         What I lack in traditional 'qualifications' Is made up by my drive to suceed and ability to learn quickly,
+          I opted for a study path that cut out the bloated and outdated schooling systems and focus on what I enjoy.
+          This allowed me to focus on modern technologies and solutions.`,
+    },
+    education: {
+      title: 'Education',
+      horeca: {
+        title: 'Hotel, Restaurant and Catering',
+        subtitle: 'Palvelualan Opisto Kuopio - 2014-2016',
+        description: `Graduated top of the class with a scholarship`,
+      },
+      android: {
+        title: 'Android Development Masterclass',
+        subtitle: 'Meicode.org - 2020',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        Sed ultricies nunc id nulla commodo tempor. Sed cursus ex sit amet lacus tempus,
+        ut molestie nulla placerat. Donec eu justo pharetra, interdum arcu vitae,
+        semper elit. Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit. Praesent eleifend vitae sapien vel scelerisque.`,
+        link: 'link holder',
+        href: '',
+      },
+      fullstack: {
+        title: 'Full-stack Javascript (MERN)',
+        subtitle: 'Highway 2 Code - Centria AMK - 2020-2021',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        Sed ultricies nunc id nulla commodo tempor. Sed cursus ex sit amet lacus tempus,
+        ut molestie nulla placerat. Donec eu justo pharetra, interdum arcu vitae,
+        semper elit. Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit. Praesent eleifend vitae sapien vel scelerisque.`,
+        link: 'link holder',
+        href: '',
+      },
+      reswebdev: {
+        title: 'Responsive Web Development',
+        subtitle: 'Freecodecamp.org - 2021',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        Sed ultricies nunc id nulla commodo tempor. Sed cursus ex sit amet lacus tempus,
+        ut molestie nulla placerat. Donec eu justo pharetra, interdum arcu vitae,
+        semper elit. Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit. Praesent eleifend vitae sapien vel scelerisque.`,
+        link: 'link holder',
+        href: '',
+      },
+    },
+  },
+  finnish: {
+    profile: 'suomi texti',
+  },
+}
--- src/components/CurriculumVitae/CVContent/CVContent.js
+++ src/components/CurriculumVitae/CVContent/CVContent.js
@@ -20,123 +20,125 @@ import {
   CVProjectBreakPoint,
   CVSchoolLogo,
 } from './CVContentElements'
+import { cvContentText } from '../../../assets/text/cvText'
 
-const CVContent = () => {
+const CVContent = ({ langauge = 'english' }) => {
   return (
     <CVContentContainer>
+      {/*   Profile   */}
       <CVElementContainer>
         <CVElementTitleWrapper>
           <CVProfileLogo />
-          <CVElementTitle>Profile</CVElementTitle>
+          <CVElementTitle>
+            {cvContentText[langauge].profile.title}
+          </CVElementTitle>
         </CVElementTitleWrapper>
         <CVElementContent>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies
-          nunc id nulla commodo tempor. Sed cursus ex sit amet lacus tempus, ut
-          molestie nulla placerat. Donec eu justo pharetra, interdum arcu vitae,
+          {cvContentText[langauge].profile.content}
         </CVElementContent>
       </CVElementContainer>
+      {/*   Education   */}
       <CVElementContainer>
         <CVElementTitleWrapper>
           <CVSchoolLogo />
-          <CVElementTitle>Education</CVElementTitle>
+          <CVElementTitle>
+            {cvContentText[langauge].education.title}
+          </CVElementTitle>
         </CVElementTitleWrapper>
+        {/*   Hotel, restaurant and catering   */}
         <CVProjectContainer>
           <CVSubtitleWrapper>
             <CVProjectBreakPoint />
-            <CVSubtitle>Restaurant, Hotel and Catering</CVSubtitle>
+            <CVSubtitle>
+              {cvContentText[langauge].education.horeca.title}
+            </CVSubtitle>
           </CVSubtitleWrapper>
           <CVProjectSubtitleWrapper>
             <CVProjectSubtitle>
-              Palvelualan Opisto Kuopio - 2014-2016
+              {cvContentText[langauge].education.horeca.subtitle}
             </CVProjectSubtitle>
           </CVProjectSubtitleWrapper>
-          <CVProjectDescriptionWrapper>
+          {/* <CVProjectDescriptionWrapper>
             <CVProjectDescription>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              ultricies nunc id nulla commodo tempor. Sed cursus ex sit amet
-              lacus tempus, ut molestie nulla placerat. Donec eu justo pharetra,
-              interdum arcu vitae, semper elit. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit. Praesent eleifend vitae sapien vel
-              scelerisque.
+              {cvContentText[langauge].education.horeca.description}
             </CVProjectDescription>
-          </CVProjectDescriptionWrapper>
-          <CVGitLinkWrapper>
-            <CVGitLogo />
-            <CVGitLink>www.gitlink.com</CVGitLink>
-          </CVGitLinkWrapper>
+          </CVProjectDescriptionWrapper> */}
         </CVProjectContainer>
+        {/*  Android Dev   */}
         <CVProjectContainer>
           <CVSubtitleWrapper>
             <CVProjectBreakPoint />
-            <CVSubtitle>Android Development Masterclass</CVSubtitle>
-          </CVSubtitleWrapper>
-          <CVProjectSubtitleWrapper>
-            <CVProjectSubtitle>Meicode.org - 2020</CVProjectSubtitle>
-          </CVProjectSubtitleWrapper>
-          <CVProjectDescriptionWrapper>
-            <CVProjectDescription>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              ultricies nunc id nulla commodo tempor. Sed cursus ex sit amet
-              lacus tempus, ut molestie nulla placerat. Donec eu justo pharetra,
-              interdum arcu vitae, semper elit. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit. Praesent eleifend vitae sapien vel
-              scelerisque.
-            </CVProjectDescription>
-          </CVProjectDescriptionWrapper>
-          <CVGitLinkWrapper>
-            <CVGitLogo />
-            <CVGitLink>www.gitlink.com</CVGitLink>
-          </CVGitLinkWrapper>
-        </CVProjectContainer>
-        <CVProjectContainer>
-          <CVSubtitleWrapper>
-            <CVProjectBreakPoint />
-            <CVSubtitle>Full-stack Javascript (MERN)</CVSubtitle>
+            <CVSubtitle>
+              {cvContentText[langauge].education.android.title}
+            </CVSubtitle>
           </CVSubtitleWrapper>
           <CVProjectSubtitleWrapper>
             <CVProjectSubtitle>
-              Highway 2 Code - Centria AMK - 2020-2021
+              {cvContentText[langauge].education.android.subtitle}
             </CVProjectSubtitle>
           </CVProjectSubtitleWrapper>
-          <CVProjectDescriptionWrapper>
+          {/* <CVProjectDescriptionWrapper>
             <CVProjectDescription>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              ultricies nunc id nulla commodo tempor. Sed cursus ex sit amet
-              lacus tempus, ut molestie nulla placerat. Donec eu justo pharetra,
-              interdum arcu vitae, semper elit. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit. Praesent eleifend vitae sapien vel
-              scelerisque.
+              {cvContentText[langauge].education.android.description}
             </CVProjectDescription>
-          </CVProjectDescriptionWrapper>
+          </CVProjectDescriptionWrapper> */}
           <CVGitLinkWrapper>
-            <CVGitLogo />
-            <CVGitLink>www.gitlink.com</CVGitLink>
+            <CVGitLink>
+              {cvContentText[langauge].education.android.link}
+            </CVGitLink>
           </CVGitLinkWrapper>
         </CVProjectContainer>
+        {/*   Full-stack  */}
         <CVProjectContainer>
           <CVSubtitleWrapper>
             <CVProjectBreakPoint />
-            <CVSubtitle>Responsive Web Development</CVSubtitle>
+            <CVSubtitle>
+              {cvContentText[langauge].education.fullstack.title}
+            </CVSubtitle>
           </CVSubtitleWrapper>
           <CVProjectSubtitleWrapper>
-            <CVProjectSubtitle>Freecodecamp.org - 2021</CVProjectSubtitle>
+            <CVProjectSubtitle>
+              {cvContentText[langauge].education.fullstack.subtitle}
+            </CVProjectSubtitle>
           </CVProjectSubtitleWrapper>
-          <CVProjectDescriptionWrapper>
+          {/* <CVProjectDescriptionWrapper>
             <CVProjectDescription>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              ultricies nunc id nulla commodo tempor. Sed cursus ex sit amet
-              lacus tempus, ut molestie nulla placerat. Donec eu justo pharetra,
-              interdum arcu vitae, semper elit. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit. Praesent eleifend vitae sapien vel
-              scelerisque.
+              {cvContentText[langauge].education.fullstack.description}
             </CVProjectDescription>
-          </CVProjectDescriptionWrapper>
+          </CVProjectDescriptionWrapper> */}
+          <CVGitLinkWrapper>
+            <CVGitLink>
+              {cvContentText[langauge].education.fullstack.link}
+            </CVGitLink>
+          </CVGitLinkWrapper>
+        </CVProjectContainer>
+        {/*   Responsive web dev   */}
+        <CVProjectContainer>
+          <CVSubtitleWrapper>
+            <CVProjectBreakPoint />
+            <CVSubtitle>
+              {cvContentText[langauge].education.reswebdev.title}
+            </CVSubtitle>
+          </CVSubtitleWrapper>
+          <CVProjectSubtitleWrapper>
+            <CVProjectSubtitle>
+              {cvContentText[langauge].education.reswebdev.subtitle}
+            </CVProjectSubtitle>
+          </CVProjectSubtitleWrapper>
+          {/* <CVProjectDescriptionWrapper>
+            <CVProjectDescription>
+              {cvContentText[langauge].education.reswebdev.description}
+            </CVProjectDescription>
+          </CVProjectDescriptionWrapper> */}
           <CVGitLinkWrapper>
             <CVGitLogo />
-            <CVGitLink>www.gitlink.com</CVGitLink>
+            <CVGitLink>
+              {cvContentText[langauge].education.reswebdev.link}
+            </CVGitLink>
           </CVGitLinkWrapper>
         </CVProjectContainer>
       </CVElementContainer>
+      {/*   Projects   */}
       <CVElementContainer>
         <CVElementTitleWrapper>
           <CVProjectsLogo />
