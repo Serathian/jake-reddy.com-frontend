ff6648fb94be61484f622a4529be4233ad7861ab
Jake <jake.a.reddy@gmail.com>
2021-06-16 20:37:17 +0300

    cv content text edited

--- src/assets/text/cvText.js
+++ src/assets/text/cvText.js
@@ -2,30 +2,20 @@ export const cvContentText = {
   english: {
     profile: {
       title: 'Profile',
-      content: `Former restaurant manager transistioning into full-stack web development. 
-      Self-taught and focusing on modern technologies and solutions, mainly MERN stack.`,
+      content: `Former restaurant manager transitioning into full-stack web development. 
+      Self-taught and focusing on modern technologies and solutions, primarily MERN stack.`,
     },
     education: {
       title: 'Education',
       horeca: {
         title: 'Hotel, Restaurant and Catering',
-        subtitle: 'Palvelualan Opisto Kuopio - 2014-2016',
+        subtitle: 'Palvelualan Opisto Kuopio',
         description: `Graduated top of the class with a scholarship`,
       },
-      android: {
-        title: 'Android Development Masterclass',
-        subtitle: 'Meicode.org - 2020',
-        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit
-        Sed ultricies nunc id nulla commodo tempor. Sed cursus ex sit amet lacus tempus,
-        ut molestie nulla placerat. Donec eu justo pharetra, interdum arcu vitae,
-        semper elit. Lorem ipsum dolor sit amet, 
-        consectetur adipiscing elit. Praesent eleifend vitae sapien vel scelerisque.`,
-        link: 'link holder',
-        href: '',
-      },
+
       fullstack: {
         title: 'Full-stack Javascript (MERN)',
-        subtitle: 'Highway 2 Code - Centria AMK - 2020-2021',
+        subtitle: 'Highway 2 Code - Centria AMK',
         description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit
         Sed ultricies nunc id nulla commodo tempor. Sed cursus ex sit amet lacus tempus,
         ut molestie nulla placerat. Donec eu justo pharetra, interdum arcu vitae,
@@ -34,9 +24,26 @@ export const cvContentText = {
         link: 'link holder',
         href: '',
       },
+    },
+    additionalstudies: {
+      title: 'Additional Studies',
       reswebdev: {
         title: 'Responsive Web Development',
-        subtitle: 'Freecodecamp.org - 2021',
+        subtitle: 'Freecodecamp.org',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        Sed ultricies nunc id nulla commodo tempor. Sed cursus ex sit amet lacus tempus,
+        ut molestie nulla placerat. Donec eu justo pharetra, interdum arcu vitae,
+        semper elit. Lorem ipsum dolor sit amet, 
+        consectetur adipiscing elit. Praesent eleifend vitae sapien vel scelerisque.`,
+        link: 'link holder',
+        href: '',
+      },
+    },
+    achivements: {
+      title: 'Achivements',
+      ggulivrr: {
+        title: 'GGULIVRR - Winning team',
+        subtitle: `Team leader of 'ScanCycle' to 1st place prize in a 2 week international app prototyping competition.`,
         description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit
         Sed ultricies nunc id nulla commodo tempor. Sed cursus ex sit amet lacus tempus,
         ut molestie nulla placerat. Donec eu justo pharetra, interdum arcu vitae,
