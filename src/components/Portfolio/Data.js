60a4885d8d14c4372c508b50c958a126a26a5b3d
Jake <jake.a.reddy@gmail.com>
2021-06-16 20:41:38 +0300

    body folder -> portfolio

---
+++ src/components/Portfolio/Data.js
@@ -0,0 +1,19 @@
+import codeImage from '../../assets/images/code-svg-1.svg'
+
+export const moreObject = {
+  id: 'more-coming',
+  lightBg: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'It wont be long',
+  headLine: `Projects coming soon`,
+  desciption: `I'm constantly working to expand my portfolio and learn new skills`,
+  show: false,
+  buttonLabel: 'Take a look!',
+  imgStart: true,
+  img: codeImage,
+  alt: 'code image',
+  dark: true,
+  primary: true,
+  darkText: false,
+}
